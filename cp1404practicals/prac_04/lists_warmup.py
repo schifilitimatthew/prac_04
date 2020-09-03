b5f44e4bc8eb64228a6a34262f72d82845fa2d76
schifillitimatthew <schifillitimatthew28@gmail.com>
2020-09-03 15:00:52 +1000

    Add warmup exercise

---
+++ cp1404practicals/prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# What values do the following expressions have?
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+# numbers[0] = "ten"
+# 2. Change the last element of numbers to 1?
+# numbers[-1] = 1
+# 3. Get all the elements from numbers except the first two (slice)
+# numbers[2:]
+# 4. Check if 9 is an element of numbers
+# 9 in numbers
